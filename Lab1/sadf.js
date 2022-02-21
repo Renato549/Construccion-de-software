f8e575f9a087c09173af6d5877cd74e699ba3c23
Renato549 <A01275401@itesm.mx>
2022-02-20 23:29:58 -0600

    Lab HTML

--- Lab1/sadf.js
+++ Lab1/sadf.js
@@ -1,5 +1,19 @@
 //variNbles, constantes, consola
 
-var numero_cervezas = 0;
+const numero_cervezas = 0;
 
 console.log(numero_cervezas);
+console.info(numero_cervezas);
+console.warn(numero_cervezas);
+console.error(numero_cervezas);
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+   
+}
+
+
+//La siguiente linea dara error porque la variable i termino en el for
+//console.log(i);
+
+console.log("Puedes tomar " + numero_cervezas + "cervezas");
