0ecc5d26292e2ecd46ada5097b23cc46c02be089
Renato549 <A01275401@itesm.mx>
2022-02-24 11:59:28 -0600

    agregar modificaciones al css

--- java.js
+++ java.js
@@ -8,13 +8,11 @@ form.addEventListener("submit", e=> {
   e.preventDefault()
   let warnings = ""
   parrafo.innerHTML = ""
+  entrar = false
   if(vpass != pass){
     warnings += "La contraseña no es valida <br>"
     entrar = true
 
-  }
-  if(entrar){
-    parrafo.innerHTML = warnings
   }
 
   
