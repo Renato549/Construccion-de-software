e6b8c06f72c6853dd164e52ee69197fc0bb21372
Renato549 <A01275401@itesm.mx>
2022-02-23 23:00:31 -0600

    agregar verificadorv2

--- java.js
+++ java.js
@@ -1,26 +1,15 @@
-const nom = document.getElementById("name")
-const email = document.getElementById("email")
+
 const pass = document.getElementById("password")
+const vpass = document.getElementById("vpassword")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warning")
 
 form.addEventListener("submit", e=> {
   e.preventDefault()
   let warnings = ""
-  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-  parrafo.innerHTML = ''
-  if(nombre.value.lenght < 3){
-    warnings += 'El nombre no es valido <br>'
-    entrar = true
-
-  }
-  if(!regexEmail.test(email.value)){
-    warnings += 'El email no es valido <br>'
-    entrar = true
-
-  }
-  if(pass.value.lenght){
-    warnings += 'El email no es valido <br>'
+  parrafo.innerHTML = ""
+  if(vpass != pass){
+    warnings += 'La contraseña no es valida <br>'
     entrar = true
 
   }
