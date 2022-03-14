7859b49c3c807aed805dc9031cf1ff0408743a8c
Renato549 <A01275401@itesm.mx>
2022-03-13 22:31:26 -0600

    cambios nombres, variables

--- lab11.js
+++ lab11.js
@@ -1,58 +1,25 @@
-
-const fs = require("fs");
-const readline = require('readline');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const r_V = require('./routes/EntV.routes');
+const r_M = require('./routes/EntA.routes');
+const path = require('path');
+
 const app = express();
 
-const bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/EntV', r_V);
+app.use('/EntA', r_M);
 
-const routeP = require('./routes/EntV.routes');
-const routeS = require('./routes/EntA.routes');
-
-app.use('/EntV', routeP);
-
-app.use('/EntA', routeS);
-
-app.use((request, response,next) => {
-    let res = '';
-    res = res +('<!DOCTYPE html>');
-    res = res +('<html lang="es-mx"><head>');
-    res = res +('<meta charset="utf-8">');
-    res = res +('<title>Lab11</title>');
-    res = res +('</head><body>');
-    res = res +('<nav')
-    res = res +('<div')
-    res = res +('<a href="/">')
-    res = res +('Lomejor que hay')
-    res = res +('</a>')
-    res = res +('<span class="navbar-text">')
-    res = res +('<b>Renato Sebastian Ramirez Calva</b><br>Lab11')
-    res = res +('</span></div></nav>')
-    res = res +('<div>')
-    res = res +('<h1>Aqui esta lo chido que entretiene </h1><br>');
-    res = res +('<br>');
-    res = res +('<p>Peliculas, series:</p>');
-    res = res +('<a href="/EntV"><button type="button">Dale a las peliculas y series</button></a>');
-    res = res +('<br><br>');
-    res = res +('<p>Música:</p>');
-    res = res +('<a href="/musica"><button type="button">Dale a la musica</button></a>');
-    res = res +('<br><br>');
-    res = res + ('</div>')
-    res = res + ('</body>');
-     
-    response.send(res)
+app.use((request, response, next) => {
+    response.render('preg');
     next();
-
-});
-
-app.use((request, response,next) => {
-    console.log("Error 404")
-    response.statusCode = 404;
-    response.send("Esto no existe bye");
 });
 
 app.listen(3000);
-
