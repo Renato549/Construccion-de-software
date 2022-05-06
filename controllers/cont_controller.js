739c383f263bfa5ae67ce098ccf8295e42371fb4
Renato549 <A01275401@itesm.mx>
2022-05-06 13:56:59 -0500

    lab18

--- controllers/cont_controller.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-exports.cont = (request, response, next) => {
-    response.render('preg', {
-    username: request.session.username ? request.session.username : '',
-    }); 
-}
